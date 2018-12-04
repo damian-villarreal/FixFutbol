a0f03bdee556cf2fe318d66a83e733d9e0888128
ferezecarr <ferezecarr@gmail.com>
2018-12-04 11:25:53 -0300

    Subiendo test

--- src/test/java/ar/edu/unlam/tallerweb1/modelo/TablaTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/modelo/TablaTest.java
@@ -1,5 +1,35 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
-public class TablaTest {
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
+import ar.edu.unlam.tallerweb1.SpringTest;
+
+public class TablaTest extends SpringTest {
+
+	private Tabla tablaUno , tablaDos , tablaTres;
+	private Equipo equipoUno , equipoDos , equipoTres;
+	private Torneo torneoUno , torneoDos , torneoTres;
+	
+	@Before
+	public void init() {
+		tablaUno = new Tabla();
+		tablaDos = new Tabla();
+		tablaTres = new Tabla();
+		equipoUno = new Equipo();
+		equipoDos = new Equipo();
+		equipoTres = new Equipo();
+		torneoUno = new Torneo();
+		torneoDos = new Torneo();
+		torneoTres = new Torneo();
+	}
+	
+	@Test
+	@Transactional
+	@Rollback
+	public void test() {
+		
+	}
 }
